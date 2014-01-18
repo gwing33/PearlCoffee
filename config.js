13055c9b5cd769a1559c61f575fcb777dc27f330
Gerald Leenerts <gerald.leenerts@gmail.com>
2014-01-18 12:58:38 -0700

    shit, changed wrong connection str

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ var env = process.env.NODE_ENV || "development";
 
 var settings = {
   production: {
-    conn_str: 'mongodb://localhost/pearl'
+    conn_str: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/dev'
   },
   staging: {
     conn_str: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/dev'
   },
   development: {
-    conn_str: 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/dev'
+    conn_str: 'mongodb://localhost/pearl'
   }
 };
 
