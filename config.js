af614361a4901548a1d9b062ac233bd8785231e8
Gerald Leenerts <gerald.leenerts@gmail.com>
2014-01-18 17:36:45 -0700

    config change for production

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || "development";
 
 var settings = {
   production: {
-    conn_str: 'mongodb://admin:' + process.env.MONGODB_PW + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/dev'
+    conn_str: 'mongodb://admin:' + process.env.MONGODB_PW + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/prod'
   },
   staging: {
     conn_str: 'mongodb://admin:' + process.env.MONGODB_PW + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/dev'
