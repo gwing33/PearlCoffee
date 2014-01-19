7675cc1baf3872b6a567d6fb5c0b060776615a90
Gerald Leenerts <gerald.leenerts@gmail.com>
2014-01-18 17:31:07 -0700

    deleted server.js file, also modified connection str

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ var env = process.env.NODE_ENV || "development";
 
 var settings = {
   production: {
-    conn_str: 'mongodb://admin:iNpCfTwZXBTZ@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/dev'
+    conn_str: 'mongodb://admin:' + process.env.MONGODB_PW + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/dev'
   },
   staging: {
-    conn_str: 'mongodb://admin:iNpCfTwZXBTZ@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/dev'
+    conn_str: 'mongodb://admin:' + process.env.MONGODB_PW + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/dev'
   },
   development: {
     conn_str: 'mongodb://localhost/pearl'
